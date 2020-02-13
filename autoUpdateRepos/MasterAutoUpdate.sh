7e0866daff42103b9b5e3e6ac6bb00218d47ca30
schapron <schapron@centreon.com>
2020-02-13 17:41:41 +0100

    wip manage errors

--- autoUpdateRepos/MasterAutoUpdate.sh
+++ autoUpdateRepos/MasterAutoUpdate.sh
@@ -6,6 +6,7 @@
 # author:sc979
 # https://github.com/sc979
 
+error=0;
 REPLY=( $(ls | grep -i 'centreon') )
 for i in ${REPLY[@]}; do
 	echo ""
@@ -16,6 +17,10 @@ for i in ${REPLY[@]}; do
         git checkout master
         git pull
 	git stash apply
+#	if [ $? -ne 0]; then
+#		echo "FAILED";
+#		$error=1;
+#	fi;
 	cd ..
 	echo "OK"
 done
